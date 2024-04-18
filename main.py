c2fb9d80007f0fb511567b8ecdc780f16c967e22
seongyunlee <private.seongyun@gmail.com>
2024-04-18 19:17:18 +0900

    main init

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def even_list(int_list: List[int]) -> List[int]:
     Args:        int_list: A list of integer.
     Returns:        A list of even integers.
     """
-    pass
+    return [i for i in int_list if i%2==0]
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:
       """    Computes the sum of the squares of all even numbers in a lis
       Args:        even_int_list: A list of even integers.Open-Source Software Practice3
